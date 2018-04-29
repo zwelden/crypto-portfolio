707b84818b7c6b79b48eabb0fe92dfb0efed2a36
Zach Welden <zwelden@broco.com>
2018-04-29 15:14:03 -0500

    style portfolios overview page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,23 +2,34 @@
 
 @section ('content')
 
-<h2>Portfolios</h2>
-<a href="/portfolios/new">Create Portfolio</a>
+<div class="portfolio-list-wrapper">
+  <div class="leader-card">
+    <h2>Portfolio List</h2>
+  </div>
+  <div class="action-btns">
+    <a href="/portfolios/new" class="btn btn-create-portfolio">Create New Portfolio</a>
+  </div>
 
-@if (count($portfolio_data) > 0)
-  <ul>
-    @foreach ($portfolio_data as $portfolio)
-      <li>
-        <span class="portfolio-name">{{ $portfolio['name'] }} </span>
-        <span class="portfolio-value-outer"> Current Value: <span class="portfolio-value">{{ $portfolio['value'] }}</span></span>
-        <a href="/portfolios/{{ $portfolio['id'] }}">View</a>
-      </li>
-    @endforeach
-  </ul>
-@else
-  <h3>No Portfolios Found</h3>
-@endif
-
+  @if (count($portfolio_data) > 0)
+    <ul class="portfolio-list">
+      @foreach ($portfolio_data as $portfolio)
+        <li class="portfolio-list-item">
+          <a href="/portfolios/{{ $portfolio['id'] }}" class="portfolio-card">
+            <div class="portfolio-info">
+              <div class="portfolio-name"><span>{{ $portfolio['name'] }}<span></div>
+              <div class="portfolio-value-outer"><span class="portfolio-value">${{ $portfolio['value'] }}</span></div>
+            </div>
+            <div class="actions">
+              <button class="btn-view">View Portfolio</button>
+            </div>
+          </a>
+        </li>
+      @endforeach
+    </ul>
+  @else
+    <h3>No Portfolios Found</h3>
+  @endif
+</div>
 <script type="text/javascript" src="/js/test.js">
 
 </script>
